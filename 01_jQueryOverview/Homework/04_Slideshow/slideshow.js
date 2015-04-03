854f805c91cf432efe06b06cd505c2854629dd3f
stuckata <stu.kirkov@gmail.com>
2015-04-03 14:37:12 +0300

    problem 04 solved

--- 01_jQueryOverview/Homework/04_Slideshow/slideshow.js
+++ 01_jQueryOverview/Homework/04_Slideshow/slideshow.js
@@ -1,24 +1,38 @@
 $(function () {
 
     var CHANGE_IMAGE_TIME = 5000; 
-    var TRANSITION_SPEED = 100;
+    var TRANSITION_SPEED = 10;
 
     var simple_slideshow = $("#slideShow");
     var images = simple_slideshow.children('li');
     var listLength = images.length;
     var i = 0;
 
-        changeList = function () {
-
-            images.eq(i).fadeOut(TRANSITION_SPEED, function () {
-                i += 1;
-                if (i === listLength) {
-                    i = 0;
-                }
-                images.eq(i).fadeIn(TRANSITION_SPEED);
-            });
-        };
+    autoChangeList = function () {
+       changeImage(1);
+    };
 
     images.not(':first').hide();
-    setInterval(changeList, CHANGE_IMAGE_TIME);
+    setInterval(autoChangeList, CHANGE_IMAGE_TIME);
+
+    $("#previousButton").click(function(event) {
+        changeImage(-1);
+    });
+
+    $("#nextButton").click(function(event) {
+        changeImage(1);
+    });
+
+    function changeImage(modifier){
+        images.eq(i).fadeOut(TRANSITION_SPEED, function () {
+            i += modifier;
+            if (i === listLength) {
+                i = 0;
+            }
+            if (i === -1) {
+                i = listLength - 1;
+            }
+            images.eq(i).fadeIn(TRANSITION_SPEED);
+        });
+    };
 });
