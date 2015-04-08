165960791ac80987c89978ae2aa1b8122e27ce2c
stuckata <stu.kirkov@gmail.com>
2015-04-08 16:58:32 +0300

    homework problem 01 solved

--- 03_WebStorages/Homework/03_CookiesAndWebStorages/greeting-system-app.js
+++ 03_WebStorages/Homework/03_CookiesAndWebStorages/greeting-system-app.js
@@ -1,1 +1,54 @@
-// JavaScript source code
+'use strict';
+
+(function () {
+
+	function arrayContains(array, value) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i] === value) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	var userName = localStorage.getItem("name") || undefined;
+	var counter = localStorage.getItem("count") || 0;
+	var userSessionsCount = localStorage.getItem(userName) || 1;
+	var users = localStorage.getItem("users") || null;
+
+	$("#greetingAlert").hide();
+
+	if (localStorage.name != undefined) {
+		$("#inputForm").hide();
+		$("#logoutButton").hide();
+		$("#greeting").append(', ' + localStorage.name + '! Your visits count: ' + userSessionsCount +
+			' Total visits count: ' + counter);
+		$("#greetingAlert").show();
+		$("#logoutButton").show();
+	};
+
+	$("#submitButton").click(function(event) {
+		userName = $("#inputName").val();
+		counter++;
+		localStorage.setItem("count", counter);
+		localStorage.setItem("name", userName);
+		var usersData = JSON.parse(localStorage.getItem("users")) || [];
+
+		if (arrayContains(usersData, userName)) {
+			userSessionsCount++;
+			localStorage.setItem(userName, userSessionsCount);
+		} else {
+			usersData.push(userName);
+			users = JSON.stringify(usersData);
+			localStorage.setItem("users", users);
+		}		
+	});
+
+	$("#logoutButton").click(function(event) {
+		userName = localStorage.name;
+		localStorage.removeItem("name", userName);
+		$("#greetingAlert").hide();
+		$("#logoutButton").hide();
+		$("#inputForm").show();
+	});
+})();
